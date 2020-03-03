040a24aa6be0de52fa54b8d8e1d69f291b32d66a
Megan Coppins <meganlcoppins@gmail.com>
2020-03-02 17:45:07 -0700

    async functions commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const Manager = require("./Develop/lib/Manager");
-const Engineer = require("./Develop/lib/Engineer");
-const Intern = require("./Develop/lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const questions = require("./lib/questions")
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -8,11 +9,54 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./Develop/lib/htmlRenderer");
+const render = require("./lib/htmlRenderer");
+const employees = [];
 
-console.log(new Manager("Lou", 1, "brent@u.com", 214));
-console.log(new Engineer("Bob", 2, "Bob@2.com", "bobgit"));
-console.log(new Intern("Ted", 3, "Ted@a.com", "UofA"));
+// console.log(new Manager("Lou", 1, "brent@u.com", 214));
+// console.log(new Engineer("Bob", 2, "Bob@2.com", "bobgit"));
+// console.log(new Intern("Ted", 3, "Ted@a.com", "UofA"));
+
+(async function init(){
+    try {
+        const userManager = await inquirer.prompt([...questions.base, ...questions.manager]);
+        employees.push(userManager);
+        console.log(employees);
+
+        promptUser();
+    } catch (err){
+        console.log(err);
+    }
+})()
+
+async function promptUser(){
+    try{
+        const employeeType = await inquirer.prompt({
+            type: "list",
+            name: "role",
+            message: "What type of employee would you like to add?",
+            choices: ["Engineer", "Intern", "Done"]
+        });
+
+        if(employeeType.role === "Engineer"){
+            const typeEngineer = await inquirer.prompt([...questions.base, ...questions.engineer]);
+            employees.push(typeEngineer);
+
+            console.log(employees);
+            promptUser();
+        }else if (employeeType.role === "Intern"){
+            const typeIntern = await inquirer.prompt([...questions.base, ...questions.intern]);
+            employees.push(typeIntern);
+
+            console.log(employees);
+            promptUser();
+        }else{
+            const html = render(employees);
+            console.log(html);
+        }
+    } catch (err){
+        console.log(err);
+    }
+};
 
 // const html = render([
 //     new Manager();
@@ -20,8 +64,6 @@ console.log(new Intern("Ted", 3, "Ted@a.com", "UofA"));
 //     new Intern();
 // ])
 // fs.writeFile(outputPath, html, ()=>{});
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -32,13 +74,3 @@ console.log(new Intern("Ted", 3, "Ted@a.com", "UofA"));
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
